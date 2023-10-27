721dc5ee14969d4eef6a6753cfda3aced72b716b
agusm <agusmena60@gmail.com>
2023-10-27 09:41:01 -0300

    validaciones admin

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,8 @@
+ 
+ 
+ import { campoRequerido, validarNombre, validarURL } from "./helpers.js";
+
+ 
 export const login = () => {
   const isAdmin = localStorage.getItem("isAdmin");
   if (isAdmin === "true") {
@@ -16,21 +21,43 @@ export const logout = () => {
 
   location.replace("/pages/login.html");
 };
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  login();
+  let campoCodigo = document.getElementById("campoCodigo");
+  let campoNombre = document.getElementById("campoNombre");
+  let campoCategoria = document.getElementById("campoCategoria");
+  let campoDescription = document.getElementById("campoDescription");
+  let campoSrcImage = document.getElementById("campoSrcImage");
+  let campoDestacada = document.getElementById("campoDestacada");
+  let campoReleased = document.getElementById("campoReleased");
+  let campoTrailerLink = document.getElementById("campoTrailerLink");
+  let formPeliculas = document.getElementById("formPeliculas");
+
+  campoCodigo.addEventListener("blur", () => {
+    campoRequerido(campoCodigo, 1, 5);
+  });
+
+  campoNombre.addEventListener("blur", () => {
+    validarNombre(campoNombre);
+  });
+  
+  campoCategoria.addEventListener("blur", () => {
+    campoRequerido(campoCategoria, 1, 14);
+  });
+  campoDescription.addEventListener("blur", () => {
+    campoRequerido(campoDescription, 1, 45);
+  });
+  campoSrcImage.addEventListener("blur", () => {
+    validarURL(campoSrcImage);
+  });
+  campoDestacada.addEventListener("blur", () => {
+    campoRequerido(campoDestacada, 1, 14);
+  });
+  campoReleased.addEventListener("blur", () => {
+    campoRequerido(campoReleased, 1, 14);
+  });
+  campoTrailerLink.addEventListener("blur", () => {
+    validarURL(campoTrailerLink);
+  });
 });
-
-let logoutButton = document.getElementById("logOut");
-logoutButton.addEventListener("click", () => {
-  logout();
-});
-
-let campoCodigo = document.getElementById("campoCodigo")
-let campoNombre = document.getElementById("campoNombre")
-let campoCategoria = document.getElementById("campoCategoria")
-let campoDescription = document.getElementById("campoDescription")
-let campoSrcImage = document.getElementById("campoSrcImage")
-let campoDestacada = document.getElementById("campoDestacada")
-let campoReleased = document.getElementById("campoReleased")
-let campoTrailerLink = document.getElementById("campoTrailerLink")
-
--- js/helpers.js
+++ js/helpers.js
@@ -4,7 +4,6 @@ export const campoRequerido = (input, min, max) => {
       input.value.length <= max &&
       input.value.length >= min
     ) {
-      console.log("cumple cond")
       input.className = "form-control is-valid";
       return true;
     } else {
@@ -41,28 +40,15 @@ if (nombreValidado.test(input.value)&& input.value.trim(). length> 0){
 
   }
   
-
-
-  /* export function sumarioValidaciones (campoMail,campoAsunto,campoDescripcion){
-    let mensaje ='';
-    if(!validarMail(campoMail))
-    {
-      mensaje +='El mail debe ser del formato nombre@ejmplo.com <br>'
+  
+  export const validarURL = (input) => {
+    let patron = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+  
+    if (patron.test(input.value)) {
+      input.className = "form-control is-valid";
+      return true;
+    } else {
+      input.className = "form-control is-invalid";
+      return false;
     }
-    if(!campoRequerido(campoMail,5,50)){
-      console.log('campoMail')
-      mensaje += 'El mail debe contener entre 5 y 50 caracteres <br>';
-    }
-    if(!campoRequerido(campoAsunto,5,50)){
-      mensaje += 'El asunto debe contener entre 5 y 10 caracteres <br>';
-    }
-    if(!campoRequerido(campoDescripcion,10,1000)){
-      mensaje+='La descripcion debe contener entre 10 y 1000 caracteres <br>'
-    }
-    if(mensaje.length!==0){
-      return mensaje;
-    }else{
-      return '';
-    }
-  }
-   */
+  };
